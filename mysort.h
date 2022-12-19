71b547e2865d6656c183bee2a7f847a226d1a50c
alenna2004 <55256093+alenna2004@users.noreply.github.com>
2022-12-19 17:42:03 +0300

    Delete mysort.h

--- mysort.h
+++
@@ -1,8 +0,0 @@
-#include "abonent.h"
-#ifndef MYSORT
-#define MYSORT
-
-void gnome_sort(Abonent **arr, int len, const char *field, const char *dest);
-void shell_sort(Abonent **arr, int len, const char *field, const char *dest);
-
-#endif
